c07edf123e78fb08814023d82d8324c0f4b11069
millacami13 <camillaprovenzano13@gmail.com>
2025-10-01 13:56:08 +0200

    web

--- Web/vite-app/src/todolist/ToDoForm.jsx
+++ Web/vite-app/src/todolist/ToDoForm.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import { useState } from "react";
 
 const ToDoForm = () => {
+const [text,setText]=useState("");
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  }
   return (
-    <div>
-      ToDoForm
-      
-    </div>
-  )
-}
+    <form className="d-flex mb-3" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="form-control me-2"
+        style={{ width: "80%" }}
+        value={text}
+        onChange={(e)=>setText(e.target.value)}
+      ></input>
+      <button className="btn btn-primary">Aggiungi task</button>
 
-export default ToDoForm
+    </form>
+    
+  );
+};
+
+export default ToDoForm;
