b989e9fdebc84b700c3bb3fd42ac8facac39f00d
millacami13 <camillaprovenzano13@gmail.com>
2025-11-28 13:42:26 +0100

    ripasso react

---
+++ vite_app2/script.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const script = () => {
+    const rootDiv=document.getElementById("root")
+    const root=ReactDOM.createRoot(rootDiv);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default script
